4ee09c91ad92a1d3edb7c48c5764a9234eb63879
Maksim Korelskii <mkorelskii@gmail.com>
2023-02-15 23:37:56 +0300

    spec for AnswerController POST#create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :questions, only: %i[ new create show ] do
-    resources :answers, shallow: true, only: %i[ new create ]
+    resources :answers, shallow: true, only: %i[ new create show ]
   end
 end
