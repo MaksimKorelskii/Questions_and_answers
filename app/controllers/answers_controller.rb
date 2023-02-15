794b5d3028853d51edf433e95d1d825e05f43320
Maksim Korelskii <mkorelskii@gmail.com>
2023-02-15 23:37:56 +0300

    spec for AnswerController POST#create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,9 +5,22 @@ class AnswersController < ApplicationController
     @answer = @question.answers.new
   end
 
+  def create
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_question
     @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
   end
 end
