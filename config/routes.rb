0f8bb159ee1df19fad9adced86785939538c4951
Maksim Korelskii <mkorelskii@gmail.com>
2023-02-15 21:53:15 +0300

    spec for QuestionsController GET#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :questions, only: %i[ new create ] do
+    resources :answers, shallow: true, only: %i[ new create ]
+  end
 end
