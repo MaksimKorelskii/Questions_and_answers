79852f5af3556aee162b0b34897ab6accedbdde4
Maksim Korelskii <mkorelskii@gmail.com>
2023-02-15 23:13:18 +0300

    spec for AnswerController GET#new

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,13 @@
+class AnswersController < ApplicationController
+  before_action :set_question, only: %i[ new create ]
+
+  def new
+    @answer = @question.answers.new
+  end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:question_id])
+  end
+end
