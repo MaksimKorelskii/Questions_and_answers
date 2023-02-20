b59c81c81b7c79a24852b4aa6155614889c27e2c
Maksim Korelskii <mkorelskii@gmail.com>
2023-02-20 23:01:27 +0300

    refactoring

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,29 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
-  let(:question) { create(:question) }
-
-  describe 'GET #new' do
-    before { get :new, params: { question_id: question } }
-    
-    it 'assigns new Answer to @answer' do
-      expect(assigns(:answer)).to be_a_new(Answer)
-    end
-
-    it 'renders new view' do
-      expect(response).to render_template :new
-    end
-  end
+  let(:user) { create(:user) }
+  let(:question) { create(:question, author: user) }
 
   describe 'POST #create' do
+    before { login(user) }
     context 'with valid attributes' do
       it 'saves a new answer in the database' do
         expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }.to change(Answer, :count).by(1)
       end
 
-      it 'redirect to show views' do
+      it 'redirect to questions#show view' do
         post :create, params: { question_id: question, answer: attributes_for(:answer) }
-        expect(response).to redirect_to(assigns(:answer))
+        expect(response).to redirect_to(assigns(:question))
       end
     end
 
@@ -35,6 +25,37 @@ RSpec.describe AnswersController, type: :controller do
       it 're-renders new view' do
         post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
         expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let(:author) { create :user }
+    let!(:answer) { create :answer, author: author, question: question }
+
+    context "User is the author of the answer" do
+      before { login(author) }
+
+      it 'Author deletes the answer from database' do
+        expect { delete :destroy, params: { id: answer} }.to change(Answer, :count).by(-1)
+      end
+
+      it 'redirects to question/show' do
+        delete :destroy, params: { id: answer }
+        expect(response).to redirect_to question_path(answer.question)
+      end
+    end
+
+    context "User isn't the author of the answer" do
+      before { login(user) }
+
+      it 'User tries to delete the answer from database' do
+        expect { delete :destroy, params: { id: answer} }.not_to change(Answer, :count)
+      end
+
+      it 'redirects to question/show' do
+        delete :destroy, params: { id: answer }
+        expect(response).to redirect_to question_path(answer.question)
       end
     end
   end
