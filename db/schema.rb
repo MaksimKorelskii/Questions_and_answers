f7fa79486c236e10e20d749e10de4570047d691b
Maksim Korelskii <mkorelskii@gmail.com>
2023-02-28 14:37:32 +0300

    migration AddBestAnswerToAnswers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_19_123849) do
+ActiveRecord::Schema.define(version: 2023_02_28_113514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2023_02_19_123849) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "author_id", null: false
+    t.boolean "best", default: false
     t.index ["author_id"], name: "index_answers_on_author_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
