efc5e88150e08f181962776444754a268ff9ef75
Maksim Korelskii <mkorelskii@gmail.com>
2023-03-03 16:55:43 +0300

    add declaration to Question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  has_one_attached :file
+
   has_many :answers, dependent: :destroy
 
   validates :title, :body, presence: true
