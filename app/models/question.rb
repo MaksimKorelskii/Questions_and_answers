22a6cb8f41a58a6e85d41bf333e262e36b091ab6
Maksim Korelskii <mkorelskii@gmail.com>
2023-03-04 00:11:28 +0300

    feature implementation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,8 @@
 class Question < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-
-  has_one_attached :file
-
   has_many :answers, dependent: :destroy
+
+  has_many_attached :files
 
   validates :title, :body, presence: true
 end
