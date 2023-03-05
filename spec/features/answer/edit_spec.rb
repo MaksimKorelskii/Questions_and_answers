55c7c065b3b3bf59f8775e6319d61bcd703da9af
Maksim Korelskii <mkorelskii@gmail.com>
2023-03-05 23:34:14 +0300

    edit answer: author can add files

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -49,6 +49,18 @@ feature 'User can edit his answer', %q{
           expect(page).to have_content("Body can't be blank")
         end
       end
+
+      scenario 'edits his answer by adding attached files', js: true do
+        within '.answers' do
+          click_on 'Edit'
+
+          attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+          click_on 'Save'
+
+          expect(page).to have_link 'rails_helper.rb'
+          expect(page).to have_link 'spec_helper.rb'
+        end
+      end
     end
 
     context 'Not author' do
