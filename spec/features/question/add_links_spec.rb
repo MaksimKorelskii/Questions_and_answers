803b1e8f3c91754c47f54669dc01fcb5a96c87d5
Maksim Korelskii <mkorelskii@gmail.com>
2023-03-11 22:05:44 +0300

    add link to question

---
+++ spec/features/question/add_links_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature "User can add links to question", %q{
+  In order to provide additional info to my question
+  As a question's author
+  I'd like to be able to add links
+} do
+
+  given(:user) { create(:user) }
+  given(:gist_url) { 'https://gist.github.com/MaksimKorelskii/855ae3036d8ed8d2c386b09323620697' }
+
+  scenario 'User adds link when asks question' do
+    sign_in(user)
+    visit new_question_path
+
+    fill_in 'Title', with: 'Question Title'
+    fill_in 'Body', with: 'Question Body'
+
+    fill_in 'Link name', with: 'My gist'
+    fill_in 'Url', with: gist_url
+
+    click_on 'Ask'
+
+    expect(page).to have_link 'My gist', href: gist_url
+  end
+end
