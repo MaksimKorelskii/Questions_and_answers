de1e86b3ff70a353c6e396ad0acf9f19362a766c
Maksim Korelskii <mkorelskii@gmail.com>
2023-03-11 22:05:44 +0300

    add link to question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,7 @@ class QuestionsController < ApplicationController
 
   def new
     @question = Question.new
+    @question.links.new # .build 
   end
 
   def show
@@ -51,6 +52,7 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:title, :body, files: [])
+    params.require(:question).permit(:title, :body,
+                                    links_attributes: [ :name, :url ], files: [])
   end
 end
