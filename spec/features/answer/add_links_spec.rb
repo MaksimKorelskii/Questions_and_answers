5614ecf991a1f5e69a4fb40576697e18213fb6ee
Maksim Korelskii <mkorelskii@gmail.com>
2023-03-15 23:15:25 +0300

    author can add links by updating answer and question

--- spec/features/answer/add_links_spec.rb
+++ spec/features/answer/add_links_spec.rb
@@ -8,14 +8,16 @@ feature "User can add links to answer", %q{
 
   given(:gist_url) { 'https://gist.github.com/MaksimKorelskii/855ae3036d8ed8d2c386b09323620697' }
   given(:user) { create(:user) }
-  given(:question) { create(:question, author: user) }
+  given!(:author) { create(:user) }
+  given!(:question) { create(:question, author: author) }
+  given!(:answer) { create(:answer, question: question, author: author) }
 
   scenario 'User adds link when asks answer', js: true do
     sign_in(user)
     visit question_path(question)
 
     fill_in "Body", with: "Answer Body"
-    fill_in 'Link name', with: 'My gist'
+    fill_in 'Name', with: 'My gist'
     fill_in 'Url', with: gist_url
 
     click_on "Answer"
@@ -23,5 +25,22 @@ feature "User can add links to answer", %q{
     within '.answers' do
       expect(page).to have_link 'My gist', href: gist_url
     end
-  end  
+  end
+  
+  scenario 'edits his answer by adding links', js: true do
+    sign_in(author)
+    visit question_path(question)
+
+    within '.answers' do
+      click_on 'Edit'
+      click_on 'add link'
+      within '.attachable-links' do
+        fill_in 'Name', with: 'My gist'
+        fill_in 'Url', with: gist_url
+      end
+
+      click_on 'Save'
+      expect(page).to have_link 'My gist', href: gist_url
+    end
+  end
 end
--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -8,23 +8,39 @@ feature "User can add links to question", %q{
 
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/MaksimKorelskii/855ae3036d8ed8d2c386b09323620697' }
+  given!(:author) { create(:user) }
+  given!(:question) { create(:question, author: author) }
 
   scenario 'User adds links when asks question' do
-    sign_in(user)
+    sign_in(author)
     visit new_question_path
 
     fill_in 'Title', with: 'Question Title'
     fill_in 'Body', with: 'Question Body'
-
     click_on 'add link'
-    
-    within '.attachable-links' do
-      fill_in 'Name', with: 'My gist'
-      fill_in 'Url', with: gist_url
-    end
+      within '.attachable-links' do
+        fill_in 'Name', with: 'My gist'
+        fill_in 'Url', with: gist_url
+      end
 
     click_on 'Ask'
-
     expect(page).to have_link 'My gist', href: gist_url
+  end
+
+  scenario 'edits his question by adding links', js: true do
+    sign_in(author)
+    visit question_path(question)
+
+    within '.question' do
+      click_on 'Edit'
+      click_on 'add link'
+      within '.attachable-links' do
+        fill_in 'Name', with: 'My gist'
+        fill_in 'Url', with: gist_url
+      end
+
+    click_on 'Save'
+    expect(page).to have_link 'My gist', href: gist_url
+    end
   end
 end
