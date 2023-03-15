d2b3882abb04a80fe98c7002ed0faf40dbf5c89f
Maksim Korelskii <mkorelskii@gmail.com>
2023-03-15 16:24:17 +0300

    if gist link display gist

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :link do
     name { "MyString" }
-    url { "MyString" }
+    url { "https://gist.github.com/MaksimKorelskii/855ae3036d8ed8d2c386b09323620697" }
   end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe Link, type: :model do
     it { should allow_value('https://gist.github.com').for(:url) }
     it { should_not allow_value('foobar').for(:url) }
   end
+
+  describe 'gist?' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, author: user) }
+    let!(:gist) { create :link, linkable: question }
+
+    it 'should be true if the address is gist.github.com' do
+      expect(gist.gist?).to be true
+    end
+
+    it 'should be false if anything besides that' do
+      expect(Link.create!(name: 'so', url: 'https://google.com/', linkable: question).gist?).to be false
+    end
+  end
 end
