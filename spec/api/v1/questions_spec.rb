86c1b5aad9e8457009d7545e0f6469028219f9b8
Maksim Korelskii <mkorelskii@gmail.com>
2023-04-10 22:56:24 +0300

    spec tests

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 describe 'Question API', type: :request do
+  let(:author) { create(:user) }
+  let(:access_token) { create(:access_token, resource_owner_id: author.id).token }
+
   let(:headers) do
     { 'CONTENT_TYPE' => 'application/json',
       'ACCEPT'       => 'application/json' }
@@ -61,6 +64,162 @@ describe 'Question API', type: :request do
             expect(answer_response[attr]).to eq answer.send(attr).as_json
           end
         end
+      end
+    end
+  end
+
+
+
+
+  describe 'POST /api/v1/questions' do
+    let(:api_path) { '/api/v1/questions' }
+
+    it_behaves_like 'API Authorizable' do
+      let(:headers) { { 'ACCEPT' => 'application/json' } }
+      let(:method) { :post }
+    end
+
+    context 'with valid attributes' do
+      let(:question_post) do
+        post api_path, params: { access_token: access_token,
+                                 headers: headers,
+                                 question: { title: 'Question title',
+                                             body:  'Question text' } }
+      end
+
+      it '201 status' do
+        question_post
+
+        expect(response).to have_http_status(201)
+      end
+
+      it 'create a new question' do
+        expect{ question_post }.to change(Question, :count).by(1)
+      end
+    end
+
+    context 'with not valid attributes' do
+      let(:question_post) do
+        post api_path, params: { access_token: access_token,
+                                 headers: headers,
+                                 question: { title: '',
+                                             body:  '' } }
+      end
+
+      it '422 status' do
+        question_post
+
+        expect(response).to have_http_status(422)
+      end
+
+      it 'dont create a new question' do
+        expect{ question_post }.to_not change(Question, :count)
+      end
+    end
+  end
+
+  describe '/api/v1/questions/:question_id' do
+    let!(:question) { create(:question, author: author) }
+    let(:question_id) { question.id }
+    let!(:comments) { create_list(:comment, 4, commentable: question, user: author) }
+    let!(:links) { create_list(:link, 4, linkable: question) }
+    let(:question_response) { json['question'] }
+    let(:api_path) { "/api/v1/questions/#{question_id}" }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    describe 'GET' do
+      before do
+        get api_path,
+            params: { access_token: access_token },
+            headers: headers
+      end
+
+      it_behaves_like 'Status be_successful'
+
+      it_behaves_like 'Returns all public fields' do
+        let(:attributes) { %w[id title body] }
+        let(:resource) { question_response }
+        let(:object) { question }
+      end
+
+      context 'Links' do
+        let(:link) { links.first }
+        let(:link_response) { question_response['links'].last }
+
+        it_behaves_like 'API List' do
+          let(:resource) { question_response['links'] }
+          let(:list_size) { 4 }
+        end
+
+        # it_behaves_like 'Returns all public fields' do
+        #   let(:attributes) { %w[id name url] }
+        #   let(:resource) { link_response }
+        #   let(:object) { link }
+        # end
+      end
+
+      # context 'Files' do
+      #   it_behaves_like 'API List' do
+      #     let(:resource) { question_response['files'] }
+      #     let(:list_size) { 1 }
+      #   end
+      # end
+
+      context 'Comments' do
+        let(:comment) { comments.first }
+        let(:comment_response) { question_response['comments'].first }
+
+        it_behaves_like 'API List' do
+          let(:resource) { question_response['comments'] }
+          let(:list_size) { 4 }
+        end
+      end
+    end
+
+    describe 'PATCH' do
+      before do
+        patch api_path, params: { access_token: access_token,
+                                  headers: headers,
+                                  question: { title: 'Edited title text',
+                                              body: 'Edited question text' } }
+      end
+
+      it_behaves_like 'Status be_successful'
+
+      it '200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'change question text' do
+        question.reload
+
+        expect(question.body).to eq 'Edited question text'
+      end
+    end
+
+    describe 'DELETE' do
+      let(:headers) { { 'ACCEPT' => 'application/json' } }
+      let(:delete_question) do
+        delete api_path, params: { access_token: access_token },
+                                   headers: headers
+      end
+
+      it_behaves_like 'API Authorizable' do
+        let(:headers) { { 'ACCEPT' => 'application/json' } }
+        let(:method) { :delete }
+      end
+
+      it '200 status' do
+        delete_question
+
+        expect(response).to have_http_status(200)
+      end
+
+      it 'delete question' do
+        expect{ delete_question }.to change(Question, :count).by(-1)
       end
     end
   end
