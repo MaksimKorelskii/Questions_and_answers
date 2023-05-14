114e68df1cb3446158d86f1bf3330a46edcb2b78
Maksim Korelskii <mkorelskii@gmail.com>
2023-05-14 23:44:19 +0300

    unicorn

--- Gemfile
+++ Gemfile
@@ -66,6 +66,7 @@ gem 'omniauth-vkontakte'
 gem 'omniauth-rails_csrf_protection'
 
 gem "mini_racer"
+gem 'unicorn'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -89,6 +90,7 @@ group :development do
   gem 'capistrano-rvm', require: false
   gem 'capistrano-passenger', require: false
   gem 'capistrano-sidekiq', require: false
+  gem 'capistrano3-unicorn', require: false
 end
 
 group :test do
--- config/deploy.rb
+++ config/deploy.rb
@@ -4,14 +4,14 @@ lock "~> 3.17.2"
 set :application, "qna"
 set :repo_url, "git@github.com:MaksimKorelskii/Questions_and_answers.git"
 
+after 'deploy:publishing', 'unicorn:restart'
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deployer/qna"
 set :deploy_user, "deployer"
-
-set :pty, false
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -21,7 +21,7 @@ set :pty, false
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 append :linked_files, "config/database.yml", 'config/master.key'
